4001e9e5656b29757309b3a4f860876f29cd8d56
Marken Ricardo <marken@example.com>
2026-08-08 12:01:22 +0800

    hero photo

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -73,7 +73,7 @@ export function Hero() {
           </div> */}
           <div className="w-100 h-100 rounded-full overflow-hidden bg-teal-deep">
             <Image
-              src="/images/grad.jpg"
+              src="/images/grad.JPG"
               alt="Marken Ricardo"
               width={200}
               height={200}
